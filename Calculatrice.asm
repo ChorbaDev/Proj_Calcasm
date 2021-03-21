2ef42a46c8521ecf4b4c3ec15b22e3cbb7019f8f
Dhoulnoun <60320772+Dhoulnoun@users.noreply.github.com>
2021-03-21 17:43:46 +0100

    commentaires calculatrice.asm
    correction des commentaires

--- Calculatrice.asm
+++ Calculatrice.asm
@@ -12,26 +12,26 @@
 ; les valeurs autorisées sont de 0 a 65535 (FFFF)
 assume ds:data, cs:code
 data segment
-        msg1 db 0Dh,0Ah, 0Dh,0Ah, 'Entrer le premier nombre: $'
+        msg1 db 0Dh,0Ah, 0Dh,0Ah, 'Entrez le premier nombre: $'
         msg2 db "choisisez un operateur:    +  -  *  /     : $"
-        msg3 db "Entrer le deuxieme nombre: $"
+        msg3 db "Entrez le deuxieme nombre: $"
         msg4 db  0dh,0ah , 'resultat : $' 
         msg5 db  0dh,0ah , 'reste : $' 
         msg6 db  0dh,0ah , 'Division par 0 est impossible$' 
         reste dw  ?
         x dw 2,"$"
         moins      DB      ?       ; on l'utilise pour le carry flag.
-        ; operateur peuvent etre: '+','-','*','/' .
+        ; operateurs peuvent etre: '+','-','*','/' .
         opr db ?
-        ; first and second number:
+        ; premier et deuxieme nb:
         num1 dw ?
         num2 dw ?
         sn1 DB 0
         sn2 DB 0
         rest db 0
 data ends
-;;; cette macro est INSPRIRE de emu8086.inc ;;;
-; cette marco ecris un caractere dans AL et avance
+;;; cette macro est INSPRIREE de emu8086.inc ;;;
+; cette marco ecrit un caractere dans AL et avance
 insert    MACRO   car
         PUSH    AX
         MOV     AL, car
@@ -48,12 +48,12 @@ code segment
 main:
         mov ax,data
         mov ds,ax
-;affichage de msg1: Entrer le premier nombre
+;affichage de msg1: Entrez le premier nombre
 lea dx, msg1
 mov ah, 09h    
 int 21h  
 
-; avoir un nombre signee 
+; avoir un nombre signe 
 ; le resultat est enregistre dans cx
 
 call scan_num
@@ -66,12 +66,12 @@ suite:
 ; stocker le premier nombre
 mov num1, cx 
 
-; nouveau ligne
+; nouvelle ligne
 insert 0Dh
 insert 0Ah
 
 
-;pour verfier que le opr entrer est valide
+;pour verfier que l opr entrer est valide
 verif:
 ;afficher le msg2: choisisez un operateur:    +  -  *  /     : 
 lea dx, msg2
@@ -83,7 +83,7 @@ mov ah, 1h
 int 21h
 mov opr, al
 
-; nouveau ligne
+; nouvelle ligne
 insert 0Dh
 insert 0Ah
 
@@ -94,12 +94,12 @@ cmp opr, '/'
 ja verif
 
 
-; afficher le message3 : Entrer le deuxieme nombre
+; afficher le message3 : Entrez le deuxieme nombre
 lea dx, msg3
 mov ah, 09h
 int 21h  
 
-; avoir un nombre signee 
+; avoir un nombre signe 
 ; le resultat est enregistre dans cx
 
 call scan_num
@@ -133,7 +133,7 @@ je do_div
 
 
 addition:
-; a supprimer
+; a supprimer ;;;;;;;;;;;;;; DO NOT FORGET;;;;;;;;;;;;;;
 cmp sn1,1
 je decc
 inc x
@@ -167,12 +167,12 @@ multi:
 mov ax, num1
 imul num2 ; (dx:ax) = ax * num2. 
 call aff_res    ; afficher resultat
-; dx sera ignorer (calc fonctionne uniquement avec des nombres pas tres grand).
+; dx sera ignore (calc fonctionne uniquement avec des nombres pas tres grand).
 jmp fin
 
 
 do_div:
-; dx sera ignorer (calc fonctionne uniquement avec des nombres pas tres grand).
+; dx sera ignore (calc fonctionne uniquement avec des nombres pas tres grand).
 cmp num2,0  ;; verifier que le denumerateur est different de 0
 je impossible
 jmp nexttt
@@ -221,7 +221,7 @@ insert '-'
 
 divis:
 idiv num2  ; ax = (dx ax) / num2.
-cmp ax,0 ; comparer le resultat de division avec 0, si c le cas on decale par un caractere et on le retire pour eviter d'afficher -0
+cmp ax,0 ; comparer le resultat de la division avec 0, si c est le cas on decale par un caractere et on le retire pour eviter d'afficher -0
 je supp_moins       
 jmp nextt
 supp_moins:
@@ -235,17 +235,17 @@ nextt:
 call aff_res    ; afficher resultat
 jmp fin
 
-aff_reste:
+aff_reste:      ;afficher le reste de la division
 call aff_res  
 mov BL,DL
 CALL CHANGE    ;changer la forme du reste 
 lea dx, msg5
-CALL RESULT     ;afficher le reste et on prend compte de retenue
+CALL RESULT     ;afficher le reste et on prend compte la retenue
 jmp fin
 
 ;;; scan_num est INSPRIRE de emu8086.inc ;;;
 
-; avoir un nombre signee 
+; avoir un nombre signe 
 ; le resultat est enregistre dans cx
 SCAN_NUM        PROC    
         ;Sauvgarder les registres 
@@ -267,17 +267,17 @@ chiffre_suivant:
         MOV     AH, 0Eh
         INT     10h
 
-        ; verifier si il y a le signe moins 
+        ; verifier s il y a le signe moins 
         CMP     AL, '-'
         JE      set_minus
 
-        ; verifier si le caractere taper c'est l'entrer 
+        ; verifier si le caractere tape c'est entrer 
         ; si c'est le cas on a fini la saisie, sinon on passe a la verification suivante
         CMP     AL, 0Dh  
         JNE     non_entrer
         JMP     fin_saisie
 non_entrer:
-        CMP     AL, 8                   ; verifier si le touche de retour est taper
+        CMP     AL, 8                   ; verifier si le touche de retour est tapee
         JNE     verif_retour            
         MOV     DX, 0                   ; si c'est le cas, on retire le chiffre precedent 
         MOV     AX, CX                  ; division:
@@ -293,7 +293,7 @@ verif_retour:
         JMP     suppr_non_chiff
 ok_AE_0:        
         CMP     AL, '9'
-        JBE     verifer ; si le chiffre a passer tout les tests avec succes donc c'est verifier
+        JBE     verifer ; si le chiffre a passe tout les tests avec succes donc c'est verifie
 suppr_non_chiff:       
         insert    8       ; retour.
         insert    ' '     ; remplacer le caractere par ' '.
@@ -308,7 +308,7 @@ verifer:
         POP     AX
 
         ; verifier si le nombre est tres grand
-        ; (il faut que le resultat est de 16 bits)
+        ; (il faut que le resultat soit de 16 bits)
         CMP     DX, 0
         JNE     t_grand1
 
@@ -367,7 +367,7 @@ CHANGE PROC
     MOV BL,AL
     MOV BH,AH
 
-    ADD BH,30H      ; convert to ascii code
+    ADD BH,30H      ; convertir en code ascii
     MOV rest,BH
 
     MOV AH,0
@@ -378,8 +378,8 @@ CHANGE PROC
     MOV BL,AL
     MOV BH,AH
 
-    ADD BH,30h      ; convert to ascii code
-    ADD BL,30h     ; covert to ascii code
+    ADD BH,30h      ; convertir en code ascii
+    ADD BL,30h     ; convertir en code ascii
 
     RET
 CHANGE ENDP
@@ -403,7 +403,7 @@ RESULT PROC
     RET
 RESULT ENDP
 ; cette procedure affiche le nombre dans AX
-; utiliser avec aff_res_NS pour afficher les nombres non signee:
+; utiliser avec aff_res_NS pour afficher les nombres non signes:
 aff_res       PROC   
         PUSH    DX
         PUSH    AX
@@ -415,7 +415,7 @@ aff_res       PROC
         JMP     printed
 
 not_zero:
-        ; valeur Absolu si c'est negative
+        ; valeur Absolu si c'est negatif
         CMP     AX, 0
         JNS     positive
         NEG     AX
@@ -430,14 +430,14 @@ printed:
         RET
 aff_res       ENDP
 
-; cette procedure affiche les nombres non signee
+; cette procedure affiche les nombres non signes
 aff_res_NS   PROC   
         PUSH    AX
         PUSH    BX
         PUSH    CX
         PUSH    DX
 
-        ; flag to prevent printing zeros before number:
+        ; flag pour empecher d afficher des zeros avant le nbr:
         MOV     CX, 1
 
         MOV     BX, 10000       
